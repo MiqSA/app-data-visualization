5ea6541e384d400847293817dfb677af4a4ff2d9
MiqSA <eng.miqueias.sa@gmail.com>
2021-06-25 11:07:22 -0300

    Corrige rota comex

--- api/app.py
+++ api/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from resources.vias import Vias
 from resources.products import Products
+from resources.comex import Comex
 
 
 app = Flask(__name__)
@@ -9,7 +10,7 @@ api = Api(app)
 
 api.add_resource(Vias, '/vias')
 api.add_resource(Products, '/products')
-api.add_resource(Products, '/comex')
+api.add_resource(Comex, '/comex')
 
 if __name__ == '__main__':
     app.run(debug=True)
