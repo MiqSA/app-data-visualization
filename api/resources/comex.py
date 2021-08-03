a190d5d44332d3a44e28b44e08d100a7c1e5a96d
MiqSA <eng.miqueias.sa@gmail.com>
2021-08-03 08:56:30 -0300

    Atualiza formatação json

--- api/resources/comex.py
+++ api/resources/comex.py
@@ -1,43 +1,22 @@
 from flask_restful import Resource
 import pandas as pd
-import numpy as np
-
-
-def df_formated(dataFrame):
-    format_data = []
-    for row in np.arange(len(dataFrame)):
-        dict_data = {}
-        for column in dataFrame:
-            dict_data[column] = list(dataFrame[column].values)[row]
-        format_data.append(dict_data)
-    return format_data
+import json
 
 
 class Comex(Resource):
     def get(self):
         data_f_comex = pd.read_csv('../datasets/f_comex.csv', sep=';')
         data_f_comex = data_f_comex[['ANO', 'MES', 'COD_NCM', 'SG_UF', 'VL_QUANTIDADE', 'MOVIMENTACAO']]
+        data_f_comex = data_f_comex.to_json(orient='records')
+        data_f_comex = json.loads(data_f_comex)
 
-        data_f_comex['ANO'] = data_f_comex['ANO'].apply(lambda x: str(x))
-        data_f_comex['MES'] = data_f_comex['MES'].apply(lambda x: str(x))
-        data_f_comex['COD_NCM'] = data_f_comex['COD_NCM'].apply(lambda x: str(x))
-        data_f_comex['SG_UF'] = data_f_comex['SG_UF'].apply(lambda x: str(x))
-        data_f_comex['VL_QUANTIDADE'] = data_f_comex['VL_QUANTIDADE'].apply(lambda x: str(x))
-
-        msg = df_formated(data_f_comex)
-        return msg
+        return data_f_comex
     
     
 class ComexLimiter(Resource):
     def get(self, id):
         data_f_comex = pd.read_csv('../datasets/f_comex.csv', sep=';', nrows=id)
         data_f_comex = data_f_comex[['ANO', 'MES', 'COD_NCM', 'SG_UF', 'VL_QUANTIDADE', 'MOVIMENTACAO']]
-
-        data_f_comex['ANO'] = data_f_comex['ANO'].apply(lambda x: str(x))
-        data_f_comex['MES'] = data_f_comex['MES'].apply(lambda x: str(x))
-        data_f_comex['COD_NCM'] = data_f_comex['COD_NCM'].apply(lambda x: str(x))
-        data_f_comex['SG_UF'] = data_f_comex['SG_UF'].apply(lambda x: str(x))
-        data_f_comex['VL_QUANTIDADE'] = data_f_comex['VL_QUANTIDADE'].apply(lambda x: str(x))
-
-        msg = df_formated(data_f_comex)
-        return msg
+        data_f_comex = data_f_comex.to_json(orient='records')
+        data_f_comex = json.loads(data_f_comex)
+        return data_f_comex
--- api/resources/products.py
+++ api/resources/products.py
@@ -1,22 +1,13 @@
 from flask_restful import Resource
 import pandas as pd
-import numpy as np
-
-
-def df_formated(dataFrame):
-    format_data = []
-    for row in np.arange(len(dataFrame)):
-        dict_data = {}
-        for column in dataFrame:
-            dict_data[column] = list(dataFrame[column].values)[row]
-        format_data.append(dict_data)
-    return format_data
+import json
 
 
 class Products(Resource):
     def get(self):
         data_d_sh2 = pd.read_excel('../datasets/d_sh2.xlsx')
         data_d_sh2 = data_d_sh2[['CO_NCM', 'NO_NCM_POR']]
-        data_d_sh2['CO_NCM'] = data_d_sh2['CO_NCM'] .apply(lambda x: str(x))
-        msg = df_formated(data_d_sh2)
-        return msg
+        data_d_sh2['CO_NCM'] = data_d_sh2['CO_NCM'].apply(lambda x: str(x))
+        data_d_sh2 = data_d_sh2.to_json(orient='records')
+        data_d_sh2 = json.loads(data_d_sh2)
+        return data_d_sh2
